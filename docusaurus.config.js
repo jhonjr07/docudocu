02b48abce3caa94ba03b54b606ce01a9374819e0
jhonjr07 <jhonpauccar43@gmail.com>
2025-04-23 22:32:26 -0500

    cambio

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/jhonjr07/',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentacion',
+  baseUrl: '/docosaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
